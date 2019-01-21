5b105042f6923563c229ce1556919e22767fef35
sukrit-adhikari <sukrit.adhikari@gmail.com>
2019-01-21 15:38:29 -0500

    signup progress

--- src/server/express-graphql.js
+++ src/server/express-graphql.js
@@ -8,52 +8,56 @@ let db = null;
 let $userService = null;
 
 var root = {
-    transactions: (args) => {return transaction.all(db)},
-    accounts: (args)=>{return account.all(db);},
-    createUser: (args)=>{return $userService.signup(args)},
-    login: (obj,context,info)=>{return $userService.login(obj.username,obj.password,context)},
+    transactions: (args) => { return transaction.all(db) },
+    accounts: (args) => { return account.all(db); },
+    createUser: (args) => { return $userService.signup(args) },
+    login: (obj, context, info) => { return $userService.login(obj.username, obj.password, context) },
 };
 
-export default  {
-    initialize: function(app){
+export default {
+    initialize: function (app) {
         db = app.get('db');
         $userService = new UserService(db);
     },
     unsecureAuthenticationMiddleware(app) {
         var self = this;
         const STR_SESSION = 'session';
-        app.set(STR_SESSION,{});
+        app.set(STR_SESSION, {});
         app.use(function (req, res, next) {
-            const session = console.log(app.get(STR_SESSION));
-            if(req.url===process.env.dev_api || req.url===(process.env.dev_api+'?')){
+            const session = app.get(STR_SESSION);
+            // console.log("session", session,"header-session",req.get("session"));
+            if (req.url === process.env.dev_api || req.url === (process.env.dev_api + '?')) {
                 next();
-            } else if(req.url==='/login' && req.method==='POST'){
+            } else if (req.url === '/login' && req.method === 'POST') {
                 req.url = '/api'; // Redirect to GQL
-                console.log(new Date(),"Login Attempt. Redirect to GQL",req.url);
+                console.log("Login Attempt.","Redirect to GQL");
                 next();
-            } else if (session[req.get("session")] && session[req.get("session")].length) {
+            } else if (Object.keys(session).includes(req.get(STR_SESSION))) {
+                console.log("Authenticated Request -> GQL");
                 next();
-            } else{
-                res.status(401).json({message:['Unauthorized Request.'],Location:"",Path:""});
+            } else {
+                console.log("Unauthenticated Request.");
+                res.status(401).json({ message: ['Unauthorized Request.'], Location: "", Path: "" });
             }
         });
     },
-    applyApiMiddleware: function(app){
+    applyApiMiddleware: function (app) {
         var self = this;
         this.initialize(app);
         this.unsecureAuthenticationMiddleware(app);
         app.use('/api', graphqlHTTP({
             schema: schema.schema,
-            rootValue: root, 
+            rootValue: root,
             graphiql: true,
-            context:{addSession:(key,value)=>{
-                const oldSession = app.get('session');
-                let entry = {};
-                entry[key] = value;
-                const newSession = Object.assign({},oldSession,entry);
-                app.set('session',newSession);
+            context: {
+                addSession: (key, value) => {
+                    const oldSession = app.get('session');
+                    let entry = {};
+                    entry[key] = value;
+                    const newSession = Object.assign({}, oldSession, entry);
+                    app.set('session', newSession);
+                }
             }
-        }
         }));
     }
 }
