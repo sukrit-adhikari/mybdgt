6167fe4921e8f07ca858315cfbad8172ffb8451a
sukriticc <sadhikari@icct.com>
2019-01-23 16:08:50 -0500

    change

--- src/js/action-creators/transaction.js
+++ src/js/action-creators/transaction.js
@@ -4,23 +4,22 @@ import fetching from '../graphql-client/fetching';
 export default {
   refreshTransactions: function () {
     return (dispatch, getState, apiClient) => {
-      const body = JSON.stringify({operationName: null,
-        query:`{
+      const body = JSON.stringify({
+        operationName: null,
+        query: `{
           transactions {
             id,userId,amount,comment,accountId,credit,dateAndTime
           } 
         }`});
-        console.log(body);
-
       fetching(null,
         'post',
         null,
         body)
-      .then(response => response.json(),(err)=>{return err;})
-      .then(result => {
-        result.data.transactions[0].amount = parseInt(Math.random()*1000);
-        dispatch({ type: actions.REFRESH_TRANSACTIONS, payload: { transactions: result.data.transactions } });
-      });
+        .then(response => response.json(), (err) => { return err; })
+        .then(result => {
+          result.data.transactions[0].amount = parseInt(Math.random() * 1000);
+          dispatch({ type: actions.REFRESH_TRANSACTIONS, payload: { transactions: result.data.transactions } });
+        });
     };
   }
 }
